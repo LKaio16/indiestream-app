ad42870da60dcf8f938a81d4abc56198628d604c
Felipe-Kunst <Felipe-Kunst@outlook.com>
2025-05-12 17:40:27 -0300

    Adicionando alteração de themas para outras paginas

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 import 'home_screen.dart';
 import 'login_screen.dart';
 import 'onboarding_screen.dart';
 import 'auth_service.dart';
+import 'theme_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   final isAuthenticated = await AuthService.isAuthenticated();
 
-  runApp(MyApp(isAuthenticated: isAuthenticated));
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => ThemeProvider(), // carrega o tema salvo
+      child: MyApp(isAuthenticated: isAuthenticated),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -19,12 +28,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Meu App',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.pinkAccent),
-        useMaterial3: true,
-      ),
+      theme: ThemeData.light(useMaterial3: true),
+      darkTheme: ThemeData.dark(useMaterial3: true),
+      themeMode: themeProvider.isDarkMode ? ThemeMode.dark : ThemeMode.light,
       routes: {
         '/onboarding': (context) => const OnboardingScreen(),
         '/login': (context) => const LoginScreen(),
